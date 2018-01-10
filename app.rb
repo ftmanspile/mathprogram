1e80dfa0bda9bf0e7969f356b0148b29ce89b6f4
ftmanspile <codfan15@gmail.com>
2018-01-10 13:46:01 -0500

    addition erb

--- app.rb
+++ app.rb
@@ -3,3 +3,24 @@ require "sinatra"
 get '/' do
 	erb :first
 end
+
+post '/math' do
+	math_answer = params[:math_type]
+	redirect '/math_input?math_answer=' + math_answer
+end
+
+get '/math_input' do
+	result = params[:math_answer]
+	if result == "addition"
+		erb :addition
+	elsif result == "subtraction"
+		erb :subtraction
+	elsif result == "division"
+		erb :division
+	elsif result == "multiplication"
+		erb :multiplication
+	else
+		puts "Your answer must be addition, subtraction, multiplication, or division."
+			
+	end
+end
